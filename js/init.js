bb8d6c96c70df0122ec89394134ea2dcf13fd549
vmshalkey <vmshalkey@gmail.com>
2016-01-07 17:48:04 -0800

    reformatted form submit page

--- js/init.js
+++ js/init.js
@@ -5,5 +5,12 @@
 
     $(".button-collapse").sideNav();
 
+// Sliders
+	$('.slider').slider({
+		full_width: false,
+		interval:2500,
+		transition:800,
+	});
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
